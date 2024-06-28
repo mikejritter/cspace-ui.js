548bb1c06f184ea570ec7a0f2114b714840b784c
Michael Ritter <emericaskate15@gmail.com>
2024-06-28 13:58:52 -0600

    Update record type

--- src/plugins/recordTypes/claim2/idGenerators.js
+++ src/plugins/recordTypes/claim2/idGenerators.js
@@ -1,12 +1,12 @@
 import { defineMessages } from 'react-intl';
 
 export default {
-  nagpraclaim: {
+  claim2: {
     csid: 'c4045ef4-a934-4a2e-8f92-edbf4069b64b',
     messages: defineMessages({
       type: {
-        id: 'idGenerator.claim.type',
-        defaultMessage: 'Claim2',
+        id: 'idGenerator.claim2.type',
+        defaultMessage: 'Claim',
       },
     }),
   },
