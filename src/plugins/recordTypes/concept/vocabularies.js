8f9ad3eda3e648ef1028d58fec3410327e4d8e33
Michael Ritter <mike.ritter@lyrasis.org>
2025-03-17 13:40:39 -0600

    Rollback ethculture concept rename for pahma

--- src/plugins/recordTypes/concept/vocabularies.js
+++ src/plugins/recordTypes/concept/vocabularies.js
@@ -156,28 +156,6 @@ export default {
       servicePath: 'urn:cspace:name(occasion)',
     },
   },
-  ethculture: {
-    messages: defineMessages({
-      name: {
-        id: 'vocab.concept.ethculture.name',
-        description: 'The name of the vocabulary.',
-        defaultMessage: 'Cultural Group',
-      },
-      collectionName: {
-        id: 'vocab.concept.ethculture.collectionName',
-        description: 'The name of a collection of records from the vocabulary.',
-        defaultMessage: 'Cultural Groups',
-      },
-      itemName: {
-        id: 'vocab.concept.ethculture.itemName',
-        description: 'The name of a record from the vocabulary.',
-        defaultMessage: 'Cultural Group',
-      },
-    }),
-    serviceConfig: {
-      servicePath: 'urn:cspace:name(ethculture)',
-    },
-  },
   archculture: {
     messages: defineMessages({
       name: {
